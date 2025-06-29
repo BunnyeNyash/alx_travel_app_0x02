508e92039f7174891a9840f2d58a585b7258f253
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-29 22:31:13 +0300

    Update settings.py

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -163,3 +163,10 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+# Celery settings
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
