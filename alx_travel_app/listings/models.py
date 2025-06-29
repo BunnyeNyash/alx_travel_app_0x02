2769880dd117c805e48631eb008edaa5a6f406e9
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-29 21:31:37 +0300

    Update models.py

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from .booking import Booking
 
 # Create your models here.
 # listings/models.py
@@ -42,3 +44,22 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.listing.title} ({self.rating}/5)"
+
+
+class Payment(models.Model):
+    STATUS_CHOICES = (
+        ('PENDING', 'Pending'),
+        ('COMPLETED', 'Completed'),
+        ('FAILED', 'Failed'),
+    )
+    
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name='payments')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_id = models.CharField(max_length=100, unique=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Payment {self.transaction_id} - {self.status}"
